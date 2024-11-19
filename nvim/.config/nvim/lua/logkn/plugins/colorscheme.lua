3cbb63a5a281e1546e1eb535d8752796d75dda5a
Logan Knapp <logkna@gmail.com>
2024-11-19 10:50:25 -0500

    kitty theme has os, extra plugins for neovim

--- nvim/.config/nvim/lua/logkn/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/logkn/plugins/colorscheme.lua
@@ -8,6 +8,9 @@ return {
     -- Like many other themes, this one has different styles, and you could load
     vim.cmd.colorscheme 'rose-pine-moon'
 
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+
     -- You can configure highlights by doing something like:
     -- vim.cmd.hi 'Comment gui=none'
   end,
