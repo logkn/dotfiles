8e7638e965f6cfd42901cfcd2d146816e900ebaf
lknapp-gp <lknapp@g-p.com>
2024-11-18 21:07:13 -0500

    nvim updated

--- nvim/.config/nvim/lua/logkn/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/logkn/plugins/colorscheme.lua
@@ -1,17 +1,18 @@
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
+  'rose-pine/neovim',
+  name = 'rose-pine',
   priority = 1000,
 
   init = function()
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
-    vim.cmd.colorscheme 'catppuccin-macchiato'
+    vim.cmd.colorscheme 'rose-pine-moon'
 
     -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
+    -- vim.cmd.hi 'Comment gui=none'
   end,
   opts = {
-    transparent_background = true,
+    -- transparent_background = true,
+    -- variant = 'moon',
   },
 }
