3c1f7950545d1a23908d715695645efa65f5dfaf
lknapp-gp <lknapp@g-p.com>
2024-11-25 13:08:00 -0500

    edit

--- nvim/.config/nvim/lua/logkn/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/logkn/plugins/nvim-tree.lua
@@ -1,11 +1,14 @@
 return {
-  'nvim-tree/nvim-tree.lua',
-  version = '*',
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
   lazy = false,
   requires = {
-    'nvim-tree/nvim-web-devicons',
+    "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require('nvim-tree').setup {}
+    require("nvim-tree").setup({})
+
+    vim.api.nvim_set_hl(0, "NvimTreeNormal", { guibg = NONE, ctermbg = NONE })
+    vim.api.nvim_set_hl(0, "NvimTreeEndOfBuffer", { guibg = NONE, ctermbg = NONE })
   end,
 }
