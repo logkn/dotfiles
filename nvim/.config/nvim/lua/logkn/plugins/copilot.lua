4dc9494cf6af4be320d0ed260f5ad8f1159b216b
lknapp-gp <lknapp@g-p.com>
2024-12-26 11:24:37 -0500

    something with copilot

--- nvim/.config/nvim/lua/logkn/plugins/copilot.lua
+++ nvim/.config/nvim/lua/logkn/plugins/copilot.lua
@@ -1,17 +1,11 @@
 return {
-  'zbirenbaum/copilot-cmp',
-  event = 'InsertEnter',
-  config = function()
-    require('copilot_cmp').setup()
-  end,
-  dependencies = {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    config = function()
-      require('copilot').setup {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      }
-    end,
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+      markdown = true,
+      help = true,
+    },
   },
 }
