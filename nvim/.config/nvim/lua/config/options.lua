5308379c6e97b645939961f43da77d0ef0283ee8
lknapp-gp <lknapp@g-p.com>
2024-12-27 12:17:12 -0500

    loaded options from old config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,71 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- OPTIONS
+local o = vim.opt
+
+-- Make line numbers default
+o.number = true
+o.relativenumber = true
+
+-- True colors
+o.termguicolors = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+o.mouse = "a"
+
+-- Enable text wrapping
+o.wrap = true
+-- o.textwidth = 88
+
+-- Don't show the mode, since it's already in the status line
+o.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  o.clipboard = "unnamedplus"
+end)
+
+-- Enable break indent
+o.breakindent = true
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+
+-- Save undo history
+o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+o.ignorecase = true
+o.smartcase = true
+
+-- Keep signcolumn on by default
+o.signcolumn = "yes"
+
+-- Decrease update time
+o.updatetime = 50
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+o.splitright = true
+o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+o.list = true
+o.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+o.inccommand = "split"
+
+-- Show which line your cursor is on
+o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+o.scrolloff = 10
