66686189f361481fb42c66bccade6f94ebcbe6c4
Logan Knapp <logkna@gmail.com>
2025-03-05 10:53:52 -0500

    avante use claude

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -5,7 +5,21 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    provider = "copilot",
+    provider = "claude",
+    openai = {
+      endpoint = "https://api.openai.com/v1",
+      model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+      timeout = 30000, -- timeout in milliseconds
+      temperature = 0, -- adjust if needed
+      max_tokens = 4096,
+      -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+    },
+    claude = {
+      endpoint = "https://api.anthropic.com",
+      model = "claude-3-7-sonnet-latest",
+      temperature = 0,
+      max_tokens = 4096,
+    },
     hints = { enabled = false },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
