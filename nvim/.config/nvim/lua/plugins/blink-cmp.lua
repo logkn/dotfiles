eb76b9f01047e7faeed23dc5aabccd4f919f5ee8
Logan Knapp <logkna@gmail.com>
2025-03-21 12:15:38 -0400

    blink start first cmdline

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -56,7 +56,7 @@ return {
         list = {
           selection = {
             -- When `true`, will automatically select the first item in the completion list
-            preselect = true,
+            preselect = false,
             -- When `true`, inserts the completion item automatically when selecting it
             auto_insert = true,
           },
