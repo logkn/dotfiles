b4b9fd4b08ab9fd05d23f6611b83303f4a6df87b
Logan Knapp <logkna@gmail.com>
2025-04-12 14:34:52 -0400

    hypr and rofi

---
+++ fish/.config/fish/functions/openmanus.fish
@@ -0,0 +1,4 @@
+function openmanus --wraps='/run/media/logan/storage/Development/OpenSource/OpenManus/.venv/bin/python /run/media/logan/storage/Development/OpenSource/OpenManus/main.py' --description 'alias openmanus=/run/media/logan/storage/Development/OpenSource/OpenManus/.venv/bin/python /run/media/logan/storage/Development/OpenSource/OpenManus/main.py'
+  /run/media/logan/storage/Development/OpenSource/OpenManus/.venv/bin/python /run/media/logan/storage/Development/OpenSource/OpenManus/main.py $argv
+        
+end
