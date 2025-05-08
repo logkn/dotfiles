26d34e37e55d15e696acf0737609ff4bfbcfc805
lknapp-gp <lknapp@g-p.com>
2025-05-08 17:19:53 -0400

    stuff

--- fish/.config/fish/functions/uv-init.fish
+++ fish/.config/fish/functions/uv-init.fish
@@ -1,4 +1,3 @@
 function uv-init --wraps='uv pin python 3.12 && uv init && rm hello.py' --wraps='uv python pin 3.12 && uv init && rm hello.py' --description 'alias uv-init=uv python pin 3.12 && uv init && rm hello.py'
-  uv python pin 3.12 && uv init && rm hello.py $argv
-        
+    uv python pin 3.12 && uv init && rm main.py $argv
 end
