294583fed807e5315bd8c3cf6785a55fd9ce0feb
lknapp-gp <lknapp@g-p.com>
2025-05-10 10:15:06 -0400

    uv-init makes src and tests

--- fish/.config/fish/functions/uv-init.fish
+++ fish/.config/fish/functions/uv-init.fish
@@ -1,3 +1,3 @@
 function uv-init --wraps='uv pin python 3.12 && uv init && rm hello.py' --wraps='uv python pin 3.12 && uv init && rm hello.py' --description 'alias uv-init=uv python pin 3.12 && uv init && rm hello.py'
-    uv python pin 3.12 && uv init && rm main.py $argv
+    uv python pin 3.12 && uv init && rm main.py $argv && mkdir src && touch src/__init__.py && mkdir tests && touch tests/__init__.py
 end
