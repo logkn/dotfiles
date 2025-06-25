514d4a1b1902fb0ba7ab487e51535afd2ed8b7da
lknapp-gp <lknapp@g-p.com>
2025-06-25 09:54:59 -0400

    multiplex update for mac

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -1,4 +1,11 @@
-function update --wraps='yay -Syu --batchinstall --devel --timeupdate --combinedupgrade' --description 'alias update=yay -Syu --batchinstall --devel --timeupdate --combinedupgrade'
-  yay -Syu --batchinstall --devel --timeupdate --combinedupgrade $argv
-        
+function update --description 'Update system packages based on OS'
+    switch (uname)
+        case Linux
+            yay -Syu --batchinstall --devel --timeupdate --combinedupgrade $argv
+        case Darwin
+            brew upgrade $argv
+        case '*'
+            echo "Unsupported OS: "(uname)
+            return 1
+    end
 end
