03ac4ca04795dd8978e977dd59a69143d78cfb3b
lknapp-gp <lknapp@g-p.com>
2025-07-03 14:36:27 -0400

    better pairs

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -64,7 +64,7 @@ return {
     -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
     --
     -- See the fuzzy documentation for more information
-    fuzzy = { implementation = 'prefer_rust_with_warning' },
+    fuzzy = { implementation = 'rust' },
   },
   opts_extend = { 'sources.default' },
 }
