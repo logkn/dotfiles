6ccc6e41c7c15c614489b91a28b1d1d34903430a
Logan Knapp <logkna@gmail.com>
2025-08-09 11:36:13 -0400

    pbcopy works on linux

--- fish/.config/fish/functions/pbcopy.fish
+++ fish/.config/fish/functions/pbcopy.fish
@@ -1,7 +1,9 @@
 function pbcopy --description 'Cross-platform clipboard copy'
     if test (uname) = Darwin
         command pbcopy $argv
+    else if test "$XDG_SESSION_TYPE" = wayland
+        wl-copy $argv
     else
-        xclip -selection clipboard $argv
+        xclip -selection clipboard -i $argv
     end
 end
