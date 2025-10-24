869d50bde4d384f4335f82815a853d942efd4b7e
lknapp-gp <lknapp@g-p.com>
2025-10-24 15:23:29 -0400

    add codex to update

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -2,8 +2,10 @@ function update --description 'Update system packages based on OS'
     switch (uname)
         case Linux
             yay -Syu --batchinstall --devel --timeupdate --combinedupgrade $argv
+            npm install -g @openai/codex
         case Darwin
             brew upgrade $argv
+            npm install -g @openai/codex
         case '*'
             echo "Unsupported OS: "(uname)
             return 1
