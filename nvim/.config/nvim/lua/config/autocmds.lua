2475fad40fe945aa366ddb9242e2c5dfc5678bee
Logan Knapp <logkna@gmail.com>
2026-01-29 11:03:10 -0500

    better tabs

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -16,6 +16,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'markdown',
   callback = function(event)
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
     vim.schedule(function()
       require('noice.text.markdown').keys(event.buf)
     end)
