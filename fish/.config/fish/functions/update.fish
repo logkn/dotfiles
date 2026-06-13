6bc6eeda6f525a265ea6c8c6103c0e4822d3f3a7
Logan Knapp <logkna@gmail.com>
2026-06-13 09:46:45 -0400

    fix timeupdate problem with update fish cmd

--- fish/.config/fish/functions/update.fish
+++ fish/.config/fish/functions/update.fish
@@ -1,7 +1,7 @@
 function update --description 'Update system packages based on OS'
     switch (uname)
         case Linux
-            yay -Syu --batchinstall --devel --timeupdate --combinedupgrade $argv
+            yay -Syu --batchinstall --devel --combinedupgrade $argv
             npm install -g @openai/codex
         case Darwin
             brew upgrade $argv
